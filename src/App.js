4403748152048617f48b1aac91e13a0dadafab6e
roshanmettu <roshanm@uw.edu>
2024-03-01 18:12:10 -0800

    march1add

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Homepage from './components/Homepage';
+import NavigationBar from './components/NavigationBar';
+
+function App() {
+  return (
+    <div>
+      <NavigationBar />
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js'
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
