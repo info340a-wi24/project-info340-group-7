aa23ee2eb8c6adb926e4528852e00c2076abecf0
Megan Pham <meganpham@Megans-MacBook-Air-52.local>
2024-03-12 21:25:01 -0700

    cosmetic changes to my page

--- src/sjTableRow.js
+++ src/sjTableRow.js
@@ -6,10 +6,10 @@ export default function SJTableRow ({title, company, location, pay, qualificatio
         <td className='tablerows'>
             <h3 className="jobdesc">{title}</h3>
             <p className="companyloca">{company}</p>
-            <p className="companyloca">Location: {location}</p>
-            <p className="companyloca">Pay: {pay}</p>
-            <p className="companyloca">Minimum Qualifications: {qualifications}</p>
-            <p className="deadline">{posted}</p>
+            <p className="companyInfo">Location: {location}</p>
+            <p className="companyInfo">Pay: {pay}</p>
+            <p className="companyInfo">Minimum Qualifications: {qualifications}</p>
+            <p className="deadline">Posted {posted}</p>
             <button type="button" className='action' > Apply <i aria-label="button" className="fas fa-pencil-alt"></i></button>
             <button type="button" className='action' onClick={onRemove}>Remove <i aria-label="button" className="fas fa-trash"></i></button>
         </td>
